53c62a0e69254aacac294be2e052c701f380dacc
Oscar Friske <oscar.friske@stud.htwk-leipzig.de>
2025-01-06 15:46:24 +0100

    initial commit

---
+++ subGridTest.hs
@@ -0,0 +1,5 @@
+test = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+getSubgridAtPosition board row column = concatMap (take 3 . drop column) (take 3 (drop row board))
+
+getAllSubgrids board = concatMap (\row -> map (\column -> getSubgridAtPosition board row column) [0, 3, 6]) [0, 3, 6]
